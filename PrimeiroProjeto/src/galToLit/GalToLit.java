c8b9ec813a967c1e75a0a86fe7ce94d5704ee55a
Aluno <Aluno@10.85.198.38>
2024-03-23 14:30:11 -0300

    projeto funcionando

--- PrimeiroProjeto/src/galToLit/GalToLit.java
+++ PrimeiroProjeto/src/galToLit/GalToLit.java
@@ -1,13 +1,19 @@
 
+
 package galToLit;
+import java.util.Scanner;
+
 public class GalToLit {
 public static void main(String[] args) {
 // declare as variáveis
 double gallons=10;
-double liters=0;
+double liters=3.785;
+var gallonsequals = (gallons*liters);
 // adicione o cálculo aqui
+Scanner in = new Scanner(System.in);
 // exiba o resultado para o usuário
 System.out.println(gallons+" gallons equals "+liters+" liters");
+System.out.println(+gallonsequals++);
 }
 }
 
